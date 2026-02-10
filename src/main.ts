28239e9c1594cdd2e134a96ce2fb861e4882ea3f
Gaston Rodriguez <gaston.rodriguez@icloud.com>
2026-02-09 19:19:02 -0500

    Modificara pipeline y secrets

--- src/main.ts
+++ src/main.ts
@@ -117,11 +117,13 @@ async function bootstrap() {
   /**
    * Start server
    */
-  const port = Number(envs.PORT) || 3001;
-  await app.listen(port, '0.0.0.0');
+  const cloudPort = Number(process.env.PORT) || 8080;
+  const internalPort = 3001;
 
-  logger.log(`App running on port ${port}`);
-  logger.log(`Swagger docs: http://localhost:${port}/api`);
+  await app.listen(cloudPort, '0.0.0.0');
+
+  logger.log(`App running on port ${internalPort}`);
+  logger.log(`Swagger docs: http://localhost:${internalPort}/api`);
 }
 
 bootstrap();
